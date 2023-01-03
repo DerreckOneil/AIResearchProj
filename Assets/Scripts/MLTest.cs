59f3915fd3204ad4cd4352aa0c42ba4385bf46ca
Derreck Oneil <derreckoneil@gmail.com>
2023-01-03 01:27:08 -0500

    Made major changes to the MLTest script. TODO: Research the behavior Parameters and ensure that the inspector has the correct information regarding actions models etc.

--- Assets/Scripts/MLTest.cs
+++ Assets/Scripts/MLTest.cs
@@ -9,24 +9,51 @@ public class MLTest : Agent
 {
     [SerializeField] private GameObject goal;
     [SerializeField] private Transform startPos;
+    float distanceToTarget;
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(gameObject.transform.position);
+        sensor.AddObservation(gameObject.transform.position); //Where I am
         sensor.AddObservation(gameObject.transform.rotation.y); //Will be the rotation in which the agent is facing.
-        sensor.AddObservation(goal.transform.position);
+        sensor.AddObservation(goal.transform.position); //Where the goal is
     }
 
 
     public override void OnActionReceived(ActionBuffers actions) //Need to study what can be done here...
     {
+        float previousDistanceToTarget = distanceToTarget;
+        distanceToTarget = Vector3.Distance(gameObject.transform.position, goal.transform.position);
+        if(gameObject.transform.position != goal.transform.position)
+        {
+            Debug.Log("Move towards the target!");
+            Vector3.MoveTowards(gameObject.transform.position, goal.transform.position, 1f); //Move towards the goal and don't stop until 1 unit before.
+        }
+        else
+        {
+            Debug.Log("We're done here");
+        }
+        
+        if(distanceToTarget > previousDistanceToTarget) //Meaning the AI is going in reverse
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+        else
+        {
+            SetReward(0.1f);
+        }
         
     }
 
-    public override void OnEpisodeBegin()
+    public override void OnEpisodeBegin() //Actually works kinda like an onEnable(). Starts at the beginning of runtime
     {
+        Debug.Log("Does this work like start or onenable?"); 
+        distanceToTarget = Vector3.Distance(gameObject.transform.position, goal.transform.position);
         gameObject.transform.position = startPos.position; //Teleport back and try again
     }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
 
+    }
     // Start is called before the first frame update
     void Start()
     {
